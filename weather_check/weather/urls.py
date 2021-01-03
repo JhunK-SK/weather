c80d33cb1123bc32830341f9f595059bfc2f9765
Jeonghun Kim <kim.jeonghun88@gmail.com>
2021-01-03 21:34:54 +0100

    finished, other functions can be added.

--- weather_check/weather/urls.py
+++ weather_check/weather/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    # Home Page
-    path('', views.index, name='home'),
+    # Home Page.
+    path('', views.index, name="home"),
+    
+    # Delete seleted city.
+    path('delete_city/<city_id>/', views.delete_city, name="delete_city"),
+    
+    # Delete all cities.
+    path('delete_all/', views.delete_all_cities, name="delete_all_cities"),
 ]
--- weather_check/weather/views.py
+++ weather_check/weather/views.py
@@ -1,6 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import requests
+from .models import City
+from .forms import CityForm
 
 def index(request):
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}"
+    api_key = "3305a1cea0761d956da3c5f87c21b563"
     
-    context = {}
+    form = CityForm()
+    
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            typed_city = form.cleaned_data["name"]
+            print(typed_city)
+            count_type_city = City.objects.filter(name=typed_city).count()
+            print(count_type_city)
+            if count_type_city == 0:
+                r = requests.get(url.format(typed_city, api_key)).json()
+                if r["cod"] == 200:
+                    form.save()
+                    return redirect("home")
+    
+    cities = City.objects.all().order_by('-created_date')
+    city_data = []
+    for city in cities:
+        
+        r = requests.get(url.format(city, api_key)).json()
+        weather_data = {
+            "id": city.id,
+            "city": r["name"],
+            "country": r["sys"]["country"],
+            "description": r["weather"][0]["description"],
+            "icon": r["weather"][0]["icon"],
+            "temp": "{:.1f}".format(r["main"]["temp"]),
+            "feelslike": "{:.1f}".format(r["main"]["feels_like"]),
+        }
+        city_data.append(weather_data)
+    
+    context = {
+        "city_data": city_data,
+        "form": form,
+        }
     return render(request, 'weather/index.html', context)
+
+
+def delete_city(request, city_id):
+    City.objects.get(pk=city_id).delete()
+    return redirect('home')
+
+def delete_all_cities(request):
+    City.objects.all().delete()
+    return redirect('home')
